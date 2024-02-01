cb05b0b641713962ceedcbbd2a6898b3df4003f5
Asad Raiyan <asadraiyan001@gmail.com>
2024-02-01 23:05:56 +0530

    added carousel sliding functionality

--- src/components/carousel/Caraousel.js
+++ src/components/carousel/Caraousel.js
@@ -12,14 +12,27 @@ import Img from "../lazyLoadImage/Img";
 import PosterFallback from "../../assets/no-poster.png";
 import CircleRating from "../circleRating/CircleRating";
 import Genres from "../genres/Genres";
+
 import "./style.scss";
 
-const Caraousel = ({ data, loading }) => {
-  const caraouselContainer = useRef();
+const Carousel = ({ data, loading, endpoint, title }) => {
+  const carouselContainer = useRef();
   const { url } = useSelector((state) => state.home);
   const navigate = useNavigate();
 
-  const navigation = (dir) => {};
+  const navigation = (dir) => {
+    const container = carouselContainer.current;
+
+    const scrollAmount =
+      dir === "left"
+        ? container.scrollLeft - (container.offsetWidth + 20)
+        : container.scrollLeft + (container.offsetWidth + 20);
+
+    container.scrollTo({
+      left: scrollAmount,
+      behavior: "smooth",
+    });
+  };
 
   const skItem = () => {
     return (
@@ -36,22 +49,29 @@ const Caraousel = ({ data, loading }) => {
   return (
     <div className="carousel">
       <ContentWrapper>
+        {title && <div className="carouselTitle">{title}</div>}
         <BsFillArrowLeftCircleFill
           className="carouselLeftNav arrow"
-          onClick={navigation("left")}
+          onClick={() => navigation("left")}
         />
         <BsFillArrowRightCircleFill
-          className="carouselRightNav arrow"
-          onClick={navigation("right")}
+          className="carouselRighttNav arrow"
+          onClick={() => navigation("right")}
         />
         {!loading ? (
-          <div className="carouselItems">
+          <div className="carouselItems" ref={carouselContainer}>
             {data?.map((item) => {
               const posterUrl = item.poster_path
                 ? url.poster + item.poster_path
                 : PosterFallback;
               return (
-                <div className="carouselItem" key={item.id}>
+                <div
+                  key={item.id}
+                  className="carouselItem"
+                  onClick={() =>
+                    navigate(`/${item.media_type || endpoint}/${item.id}`)
+                  }
+                >
                   <div className="posterBlock">
                     <Img src={posterUrl} />
                     <CircleRating rating={item.vote_average.toFixed(1)} />
@@ -60,7 +80,9 @@ const Caraousel = ({ data, loading }) => {
                   <div className="textBlock">
                     <span className="title">{item.title || item.name}</span>
                     <span className="date">
-                      {dayjs(item.release_Date).format("MMM D, YYYY")}
+                      {dayjs(item.release_date || item.first_air_date).format(
+                        "MMM D, YYYY"
+                      )}
                     </span>
                   </div>
                 </div>
@@ -81,4 +103,4 @@ const Caraousel = ({ data, loading }) => {
   );
 };
 
-export default Caraousel;
+export default Carousel;
