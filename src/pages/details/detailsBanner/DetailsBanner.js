bc798716b106b41931bae2e38ff68f6c2b284505
Asad Raiyan <asadraiyan001@gmail.com>
2024-02-02 17:41:48 +0530

    created cast component

--- src/pages/details/detailsBanner/DetailsBanner.js
+++ src/pages/details/detailsBanner/DetailsBanner.js
@@ -12,8 +12,11 @@ import CircleRating from "../../../components/circleRating/CircleRating";
 import Img from "../../../components/lazyLoadImage/Img";
 import PosterFallback from "../../../assets/no-poster.png";
 import { PlayIcon } from "../Playbtn";
+import VideoPopup from "../../../components/videoPopup/VideoPopup";
 
 const DetailsBanner = ({ video, crew }) => {
+  const [show, setShow] = useState(false);
+  const [videoId, setVideoId] = useState(null);
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}`);
   const { url } = useSelector((state) => state.home);
@@ -63,7 +66,13 @@ const DetailsBanner = ({ video, crew }) => {
                     <Genres data={_genres} />
                     <div className="row">
                       <CircleRating rating={data.vote_average.toFixed(1)} />
-                      <div className="playbtn">
+                      <div
+                        className="playbtn"
+                        onClick={() => {
+                          setShow(true);
+                          setVideoId(video.key);
+                        }}
+                      >
                         <PlayIcon />
                         <span className="text">Watch Trailer</span>
                       </div>
@@ -126,8 +135,29 @@ const DetailsBanner = ({ video, crew }) => {
                         </span>
                       </div>
                     )}
+                    {data?.created_by?.length > 0 && (
+                      <div className="info">
+                        <span className="text bold">Creator: </span>
+                        <span className="text">
+                          {data?.created_by?.map((d, i) => {
+                            return (
+                              <span key={i}>
+                                {d.name}
+                                {data?.created_by?.length - 1 !== i && ", "}
+                              </span>
+                            );
+                          })}
+                        </span>
+                      </div>
+                    )}
                   </div>
                 </div>
+                <VideoPopup
+                  show={show}
+                  setShow={setShow}
+                  videoId={videoId}
+                  setVideoId={setVideoId}
+                />
               </ContentWrapper>
             </React.Fragment>
           )}
